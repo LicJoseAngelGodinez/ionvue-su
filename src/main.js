fe6439f7b3b216845de017d6670c10fcca91cb15
José Ángel <201400189@estudiantes.upqroo.edu.mx>
2019-08-15 09:50:30 -0500

    Se inicializa el proyecto.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import VueRouter from 'vue-router';
+
+import routes from './routes';
+
+// export const router = new VueRouter({
+  //   base: '/',
+//   mode: 'history',
+//   routes: [
+  //     { path: '/path', component: component }
+//   }
+// });
 
 Vue.config.productionTip = false
 
+Vue.use(VueRouter);
+
+const router = new VueRouter({routes});
+
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: h => h(App)
+}).$mount('#app');
