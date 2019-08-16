cae5b2eef708f3afd3345cc21d4c421dafd093bc
José Ángel <201400189@estudiantes.upqroo.edu.mx>
2019-08-16 11:37:40 -0500

    Se agregan cambios de funcinalidad, ya envio los datos al scope.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,21 +2,20 @@
   <div class="ion-page">
     <ion-header>
       <ion-toolbar>
-        <ion-title text-center>SalesUp! Forms</ion-title>
+        <ion-title text-center>{{titulo}}</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content text-center class="ion-padding">
-      <h1 >Bienvenido!</h1>
-      
+      <h1>{{mensaje}}</h1>      
       <ion-item>
         <ion-label position="floating">Correo*</ion-label>
-        <ion-input autofocus="true"  clear-input="true" type="email" name="account" required="true"></ion-input>
+        <ion-input autofocus="true"  clear-input="true" type="email" id="cuenta" name="cuenta" required="true">{{cuenta}}</ion-input>
       </ion-item>
       <ion-item>
         <ion-label position="floating">Contrase&ntilde;a*</ion-label>
-        <ion-input clear-on-edit="true" type="password" name="password" required="true"></ion-input>
+        <ion-input clear-on-edit="true" type="password" id="password" name="password" required="true">{{pass}}</ion-input>
       </ion-item>
-      <ion-button expand="block" color="primary" @click="loginFunc">Acceder</ion-button>
+      <ion-button expand="block" color="primary" @click="loginBtn">Acceder</ion-button>
       <ion-button disabled=true expand="block" color="light" href='/login'>Desactivado</ion-button>
       <img alt="Vue logo" src="../assets/logo.png">
     </ion-content>
@@ -26,9 +25,30 @@
 <script>
 export default {
   name: "home",
+  data(){ return {
+      titulo: 'SalesUp! Forms',
+      mensaje: 'Bienvenido!',
+      cuenta: '',
+      pass: ''
+    }    
+  },
+  created(){
+    // eslint-disable-next-line
+    console.log(this.cuenta, this.pass);
+  },
   methods: {
-    loginFunc () {
-      //console.log('prueba');
+    loginBtn () {
+      var nptCuenta = '',
+          nptPass = '';
+          
+      nptCuenta = document.getElementById('cuenta');
+      nptPass = document.getElementById('password');
+
+      // eslint-disable-next-line
+      console.log({
+        cuenta: nptCuenta.value,
+        pass: nptPass.value
+      });
     }
   }
 };
