297860e429c156c7f09a5db423ba512af96f7b25
José Ángel <201400189@estudiantes.upqroo.edu.mx>
2019-08-16 16:58:16 -0500

    Se agregan cambios para login.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,34 +2,66 @@
   <div class="ion-page">
     <ion-header>
       <ion-toolbar>
-        <ion-title text-center>{{titulo}}</ion-title>
+        <ion-title 
+        text-center
+        >{{titulo}}</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content text-center class="ion-padding">
+    <ion-content 
+    text-center
+    class="ion-padding"
+    >
       <h1>{{mensaje}}</h1>      
       <ion-item>
-        <ion-label position="floating">Correo*</ion-label>
-        <ion-input autofocus="true"  clear-input="true" type="email" id="cuenta" name="cuenta" required="true">{{cuenta}}</ion-input>
+        <ion-label 
+        position="floating"
+        >Correo*</ion-label>
+        <ion-input 
+        autofocus="true" 
+        clear-input="true" 
+        type="email" 
+        id="cuenta" 
+        name="cuenta" 
+        required="true"
+        >{{cuenta}}</ion-input>
       </ion-item>
       <ion-item>
-        <ion-label position="floating">Contrase&ntilde;a*</ion-label>
-        <ion-input clear-on-edit="true" type="password" id="password" name="password" required="true">{{pass}}</ion-input>
+        <ion-label 
+        position="floating"
+        >Contrase&ntilde;a*</ion-label>
+        <ion-input 
+        clear-on-edit="true" 
+        type="password" 
+        id="password" 
+        name="password" 
+        required="true"
+        >{{pass}}</ion-input>
       </ion-item>
-      <ion-button expand="block" color="primary" @click="loginBtn">Acceder</ion-button>
-      <ion-button disabled=true expand="block" color="light" href='/login'>Desactivado</ion-button>
+      <ion-button 
+      expand="block" 
+      color="primary" 
+      @click="loginBtn"
+      >Acceder</ion-button>
+      <ion-button 
+      disabled=true 
+      expand="block" 
+      color="light" 
+      href='/login'
+      >Desactivado</ion-button>
       <img alt="Vue logo" src="../assets/logo.png">
     </ion-content>
   </div>
 </template>
 
 <script>
+
 export default {
   name: "home",
   data(){ return {
-      titulo: 'SalesUp! Forms',
-      mensaje: 'Bienvenido!',
-      cuenta: '',
-      pass: ''
+      titulo      : 'SalesUp! Forms',
+      mensaje     : 'Bienvenido!',
+      cuenta      : '',
+      pass        : ''
     }    
   },
   created(){
@@ -38,17 +70,36 @@ export default {
   },
   methods: {
     loginBtn () {
-      var nptCuenta = '',
-          nptPass = '';
+      var nptCuenta   = '',
+          nptPass     = '',
+          cuenta      = '',
+          pass        = '';
           
-      nptCuenta = document.getElementById('cuenta');
-      nptPass = document.getElementById('password');
+      nptCuenta       = document.getElementById('cuenta');
+      nptPass         = document.getElementById('password');
 
       // eslint-disable-next-line
       console.log({
         cuenta: nptCuenta.value,
         pass: nptPass.value
       });
+
+      cuenta          = nptCuenta.value;
+      pass            = nptPass.value;
+
+      if ( cuenta === 'xxxx' && pass === 'xxxx' ) {
+
+        // eslint-disable-next-line
+        console.log('VALIDADO');
+        this.$router.push('/login');
+
+
+      } else {
+
+        // eslint-disable-next-line
+        console.log('NO VALIDADO');
+
+      }
     }
   }
 };
